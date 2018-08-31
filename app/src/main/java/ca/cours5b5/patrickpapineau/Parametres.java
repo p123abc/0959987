e1d71ba8195abfe5142aaba7775f4c3cadc89ad2
Patrick Papineau <patpap002@gmail.com>
2018-08-31 15:18:34 -0400

    FAIT: Atelier 02 1 - Bonjours en FR Hello en EN 2 - Paysage en Mode Paysage / Portrait en mode Portrait
    TODO:
    Atelier 03

--- app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
@@ -1,5 +1,6 @@
 package ca.cours5b5.patrickpapineau;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,6 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette","Bonjours!");
+        Log.d("MonEtiquette",this.getResources().getString(R.string.IDENTIFIANT)+" "+this.getResources().getString(R.string.ORIENTATION));
     }
 }
