08e18d34da512d13a07df2232a44f9865183848e
Patrick Papineau <patpap002@gmail.com>
2018-09-05 15:51:54 -0400

    Commit aprèes que le prof est réparer VParametres

--- app/src/main/java/ca/cours5b5/patrickpapineau/vues/VParametres.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/vues/VParametres.java
@@ -6,15 +6,15 @@ import android.util.AttributeSet;
 
 public class VParametres extends ConstraintLayout{
 
-    public VMaVue(Context context){
+    public VParametres(Context context) {
         super(context);
     }
 
-    public VMaVue(Context context, AttributeSet attrs){
+    public VParametres(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public VMaVue(Context context, AttributeSet attrs, int defStyleAttr){
+    public VParametres(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 }
