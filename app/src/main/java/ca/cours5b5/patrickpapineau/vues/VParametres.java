37b460f779a199fb668999f9937d5de70b35e466
Patrick Papineau <patpap002@gmail.com>
2018-09-05 15:28:53 -0400

    FAIT: Atelier 03 1 - Modifier le SDK de 28 a 26 2 - Création des classes et des package
    TODO:
    Atelier 03
    Code la vue, le global et l'avitité
    (ce commit a été fait avant la modification du layout activity_parametres.xml

---
+++ app/src/main/java/ca/cours5b5/patrickpapineau/vues/VParametres.java
@@ -0,0 +1,20 @@
+package ca.cours5b5.patrickpapineau.vues;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class VParametres extends ConstraintLayout{
+
+    public VMaVue(Context context){
+        super(context);
+    }
+
+    public VMaVue(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    public VMaVue(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+}
