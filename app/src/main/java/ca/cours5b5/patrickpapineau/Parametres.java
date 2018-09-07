a73db0edc3cd138ac123bd893ae8c3e64502e161
Patrick Papineau <patpap002@gmail.com>
2018-09-07 15:20:09 -0400

    FAIT: Atelier 03 TODO: Atelier 04
    NOTE: Beaucoup de commit non important ont été faite

--- app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
@@ -4,6 +4,7 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
 public class Parametres extends AppCompatActivity {
 
--- app/src/main/java/ca/cours5b5/patrickpapineau/vues/VParametres.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/vues/VParametres.java
@@ -3,6 +3,10 @@ package ca.cours5b5.patrickpapineau.vues;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import ca.cours5b5.patrickpapineau.R;
 
 public class VParametres extends ConstraintLayout{
 
@@ -16,5 +20,35 @@ public class VParametres extends ConstraintLayout{
 
     public VParametres(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onFinishInflate(){
+        super.onFinishInflate();
+
+        Spinner spinnerHauteur = this.findViewById(R.id.spinnerHauteur);
+        Spinner spinnerLargeur = this.findViewById(R.id.spinnerLargeur);
+        Spinner spinnerPourGagnier = this.findViewById(R.id.spinnerPourGagnier);
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<Integer> adapterPG = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+
+        spinnerHauteur.setAdapter(adapter);
+        spinnerLargeur.setAdapter(adapter);
+        spinnerPourGagnier.setAdapter(adapterPG);
+
+        for (int i = 4; i<11; i++){
+            adapter.add(i);
+        }
+
+        adapterPG.add(3);
+        adapterPG.add(4);
+
+        spinnerHauteur.setSelection(2);
+        spinnerLargeur.setSelection(3);
+        spinnerPourGagnier.setSelection(1);
+
+
+
     }
 }
