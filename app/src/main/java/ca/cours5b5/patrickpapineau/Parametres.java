71ffb695776e740dbb20ea47afee5b3a2f034079
Patrick Papineau <patpap002@gmail.com>
2018-09-07 12:34:32 -0400

    petit update (nom des textView)

--- app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/Parametres.java
@@ -10,7 +10,7 @@ public class Parametres extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette",this.getResources().getString(R.string.IDENTIFIANT)+" "+this.getResources().getString(R.string.ORIENTATION));
+       setContentView(R.layout.activity_parametres);
+        //Log.d("MonEtiquette",this.getResources().getString(R.string.IDENTIFIANT)+" "+this.getResources().getString(R.string.ORIENTATION));
     }
 }
