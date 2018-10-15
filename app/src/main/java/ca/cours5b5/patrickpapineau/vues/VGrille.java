1a6e1e432d34eb8f0ac431efefedd24198b1bb9f
Patrick Papineau <patpap002@gmail.com>
2018-10-15 13:21:33 -0400

    Atelier 6 Update class (un autre cour commence)

--- app/src/main/java/ca/cours5b5/patrickpapineau/vues/VGrille.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/vues/VGrille.java
@@ -1,4 +1,62 @@
 package ca.cours5b5.patrickpapineau.vues;
 
+import android.content.Context;
+import android.text.Layout;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+import java.util.List;
+
 public class VGrille {
+    //TODO
+    public VGrille(Context context){
+
+    }
+
+    //TODO
+    public VGrille(Context context, AttributeSet attrs){
+
+    }
+
+    //TODO
+    public VGrille(Context context, AttributeSet attrs, int defStyleAttr){
+
+    }
+
+    private int nombreRangees;
+
+    private List<VEntete> entetes;
+
+    private VCase[][] lesCases;
+
+    // Ou avec des liste (voir l énoncé de l atelier 6 dans Classe VGrille)
+
+    //TODO
+    @Override
+    protected void onFinishInflate(){
+
+    }
+    //TODO
+    void creerGrille(int hauteur, int largeur){
+
+    }
+
+    //TODO
+    private void initialiserTableauDeCase(int hauteur, int largeur){
+
+    }
+
+    // Ou avec des liste (voir l énoncé de l atelier 6 dans Classe VGrille)
+
+    //TODO
+    private void ajouterEnTetes(int largeur){
+
+    }
+
+    //TODO
+    private LayoutParams getMiseEnPageEntete(int colonne){
+
+    }
+
+
 }
