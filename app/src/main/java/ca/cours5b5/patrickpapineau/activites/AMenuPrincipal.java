1dbcee747bfccf6d065e2da996a3d04fc1b5f900
p123abc <patpap002@gmail.com>
2018-10-18 03:06:10 -0400

    ajout du bouton dans le layout (xml) et du listener dans AMenuPrincipale

--- app/src/main/java/ca/cours5b5/patrickpapineau/activites/AMenuPrincipal.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/activites/AMenuPrincipal.java
@@ -32,11 +32,23 @@ public class AMenuPrincipal extends Activite {
                 transitionParametres();
             }
         });
+        Button bouton2 = this.findViewById(R.id.button2);
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transitionPartie();
+            }
+        });
     }
 
     private void transitionParametres(){
         Intent intentionParametres = new Intent(this, AParametres.class);
         startActivity(intentionParametres);
+    }
+
+    private void transitionPartie(){
+        Intent intentionPartie = new Intent(this, APartie.class);
+        startActivity(intentionPartie);
     }
 
 }
