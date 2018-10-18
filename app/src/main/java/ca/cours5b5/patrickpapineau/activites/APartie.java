a278659cd0445cfd878056b3efebe0d44f9b4b25
p123abc <patpap002@gmail.com>
2018-10-18 16:16:01 -0400

    mini update avant de partir travailler

--- app/src/main/java/ca/cours5b5/patrickpapineau/activites/APartie.java
+++ app/src/main/java/ca/cours5b5/patrickpapineau/activites/APartie.java
@@ -1,6 +1,16 @@
 package ca.cours5b5.patrickpapineau.activites;
 
-public class APartie {
+import android.os.Bundle;
 
+import ca.cours5b5.patrickpapineau.R;
+
+public class APartie extends Activite{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
 
 }
